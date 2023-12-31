2e6b8d77e7d1800c6969a0ce9b14b7bf177f5582
PointSource <47544779+point-source@users.noreply.github.com>
2023-12-30 20:54:36 -0800

    Make edge even more minimal

--- lib/edge.dart
+++ lib/edge.dart
@@ -5,20 +5,6 @@ part 'edge.mapper.dart';
 
 @MappableClass()
 @immutable
-class Edge with EdgeMappable {
-  final String id;
-  final Map<String, EdgeUi> layers;
-
-  Edge({
-    String? id,
-    this.layers = const {},
-  }) : id = id ?? '';
-
-  EdgeLayerView layer(String layerId) => EdgeLayerView(this, layerId);
-}
-
-@MappableClass()
-@immutable
 class EdgeUi with EdgeUiMappable {
   final String label;
   final Color color;
@@ -27,27 +13,4 @@ class EdgeUi with EdgeUiMappable {
     this.label = '',
     this.color = Colors.blue,
   });
-}
-
-class EdgeLayerView {
-  Edge edge;
-  final String layer;
-
-  EdgeLayerView(this.edge, this.layer);
-
-  EdgeUi? get ui => edge.layers[layer];
-
-  EdgeLayerView updateLayer(EdgeUi Function(EdgeUi ui) updater) {
-    final Map<String, EdgeUi> layers = Map.from(edge.layers);
-    layers[layer] = updater(layers[layer]!);
-    edge = edge.copyWith(layers: layers);
-    return this;
-  }
-
-  EdgeLayerView setLabel(String label) {
-    updateLayer((ui) => ui.copyWith(label: label));
-    return this;
-  }
-
-  Edge save() => edge;
 }
